012d4691346a24facf707fa4a72d548ce3be7171
Roger Adala <38158159+rogerBret@users.noreply.github.com>
2021-07-21 11:08:02 +0100

    Add files via upload

--- test-app-pm2/app.js
+++ test-app-pm2/app.js
@@ -1,1 +1,17 @@
+const express = require('express');
 
+const debug = require('debug')('express');
+
+const app = express();
+
+const EXPRESS_PORT = process.env.EXPRESS_PORT || 3010;
+
+app.get('/', (req, res)=>{
+    debug(`${req.headers['user-agent']} has accesses to root URL`);
+    res.send('hello from the node.js web server');
+    });
+    app.listen(EXPRESS_PORT, err =>{
+        if(!err){
+            debug(`Express web server is running at port${EXPRESS_PORT}`)
+        }
+    });
